219595acf655e7a3bcb2733a4e3a5b7d305f81ad
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-05-26 19:24:33 -0700

    Setup Angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardFormComponent } from './components/card-form/card-form.component';
+import { CardDetailComponent } from './components/card-detail/card-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent,
+    CardFormComponent,
+    CardDetailComponent
   ],
   imports: [
     BrowserModule,
