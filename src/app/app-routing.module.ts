a7c293a4475eef143cd1a6a3ad1705fd0d155504
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-06-05 01:29:51 -0700

    Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardFormComponent } from './components/card-form/card-form.component';
+import { CardDetailComponent } from './components/card-detail/card-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CardListComponent },
+  { path: 'add', component: CardFormComponent },
+  { path: 'edit/:id', component: CardFormComponent },
+  { path: 'card/:id', component: CardDetailComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
