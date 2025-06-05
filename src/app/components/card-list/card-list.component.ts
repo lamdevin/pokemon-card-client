df1a18fabf14aaf6c8fd02ec0ab54dbfe285a798
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-06-05 01:50:53 -0700

    Made basic card list component
    - Removed placeholder html

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardService } from '../../services/card-service';
+import { Card } from '../../models/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-list',
-  templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.css']
+  templateUrl: './card-list.component.html'
 })
-export class CardListComponent {
+export class CardListComponent implements OnInit {
+  cards: Card[] = [];
 
+  constructor(private cardService: CardService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadCards();
+  }
+
+  loadCards() {
+    this.cardService.getCards().subscribe(data => this.cards = data);
+  }
+
+  deleteCard(id: number) {
+    this.cardService.deleteCard(id).subscribe(() => this.loadCards());
+  }
 }
