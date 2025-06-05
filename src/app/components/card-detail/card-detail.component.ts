61572639563cfa5094b5dd1c15089c80022f660e
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-06-05 02:11:42 -0700

    Complete Card detail component

--- src/app/components/card-detail/card-detail.component.ts
+++ src/app/components/card-detail/card-detail.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardService } from '../../services/card-service';
+import { ActivatedRoute } from '@angular/router';
+import { Card } from '../../models/card';
 
 @Component({
   selector: 'app-card-detail',
-  templateUrl: './card-detail.component.html',
-  styleUrls: ['./card-detail.component.css']
+  templateUrl: './card-detail.component.html'
 })
-export class CardDetailComponent {
+export class CardDetailComponent implements OnInit {
+  card?: Card;
 
+  constructor(private cardService: CardService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.cardService.getCard(id).subscribe(card => this.card = card);
+  }
 }
