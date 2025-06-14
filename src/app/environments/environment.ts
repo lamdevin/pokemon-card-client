9ef377fc59ba1dc7272ca8f5e144d5f87fddfe7a
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-06-14 00:55:55 -0700

    Fix error when pressing cancel in edit form

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pokemoncardserver.onrender.com/api/pokemon',
+  // apiUrl: 'https://pokemoncardserver.onrender.com/api/pokemon',
+  apiUrl: 'http://localhost:8080/api/pokemon',
 };
